5d7c0c199c3082eab862aab857ec398e4193f6bc
Boochu <mayhu1993@gmail.com>
2026-09-20 23:09:19 +0800

    data: record ingredient effect strengths

--- src/data/ingredients.ts
+++ src/data/ingredients.ts
@@ -7,7 +7,12 @@ export const ingredients: Ingredient[] = [
     stage: 1,
     buyPrice: 9,
     seller: '檸檬商人',
-    effects: ['酸味', '增強免疫', '保護心臟', '輔助瘦身'],
+    effects: [
+      { name: '酸味', value: 4 },
+      { name: '增強免疫', value: 3 },
+      { name: '保護心臟', value: 1 },
+      { name: '輔助瘦身', value: 1 },
+    ],
   },
   {
     id: 'orange',
@@ -15,7 +20,13 @@ export const ingredients: Ingredient[] = [
     stage: 1,
     buyPrice: 11,
     seller: '蔬果商',
-    effects: ['增強免疫', '煥亮肌膚', '芳香', '保護心臟', '提神醒腦'],
+    effects: [
+      { name: '增強免疫', value: 4 },
+      { name: '煥亮肌膚', value: 2 },
+      { name: '芳香', value: 1 },
+      { name: '保護心臟', value: 1 },
+      { name: '提神醒腦', value: 1 },
+    ],
   },
   {
     id: 'mint',
@@ -23,7 +34,13 @@ export const ingredients: Ingredient[] = [
     stage: 2,
     buyPrice: 14,
     seller: '薄荷商人',
-    effects: ['清新口氣', '舒緩腸胃', '芳香', '紓解壓力', '增強腦力'],
+    effects: [
+      { name: '清新口氣', value: 4 },
+      { name: '舒緩腸胃', value: 3 },
+      { name: '芳香', value: 2 },
+      { name: '紓解壓力', value: 1 },
+      { name: '增強腦力', value: 1 },
+    ],
   },
   {
     id: 'sugar',
@@ -31,6 +48,9 @@ export const ingredients: Ingredient[] = [
     stage: 2,
     buyPrice: 7,
     seller: '售糖商人',
-    effects: ['甜味', '補充精力'],
+    effects: [
+      { name: '甜味', value: 5 },
+      { name: '補充精力', value: 3 },
+    ],
   },
 ]
