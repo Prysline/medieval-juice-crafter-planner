c9d5a67ed2ed962e309b750ec86add4ea72356c3
Boochu <mayhu1993@gmail.com>
2026-09-21 03:42:01 +0800

    Spike optimizer browser bundle size

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
+if (new URLSearchParams(window.location.search).has('__optimizer_probe')) {
+  void import('./domain/optimizer')
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
