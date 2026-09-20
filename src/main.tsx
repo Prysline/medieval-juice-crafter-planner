f0fa642e72079618d3d636ff334b807819ad22e7
Boochu <mayhu1993@gmail.com>
2026-09-21 03:42:53 +0800

    Remove optimizer bundle probe

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
-if (new URLSearchParams(window.location.search).has('__optimizer_probe')) {
-  void import('./domain/optimizer')
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
