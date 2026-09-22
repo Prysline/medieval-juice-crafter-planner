b861d1a56b0844e90b9579473fe7d576c2df7df4
Boochu <mayhu1993@gmail.com>
2026-09-22 18:02:16 +0800

    Correctness-2B: update validation fixtures

--- src/domain/planApplicationValidation.test.ts
+++ src/domain/planApplicationValidation.test.ts
@@ -41,6 +41,7 @@ function basis(): PlanApplicationBasisState {
       juiceJarCarryMode: 'fixed-slots',
       reservedJuiceJarSlots: 2,
       allowUsedCupDropIfFull: false,
+      allowDiscardRetainedJuice: false,
     },
   }
 }
@@ -74,6 +75,8 @@ function draftFromBasis(
           source.plannerSettings.reservedJuiceJarSlots,
         allowUsedCupDropIfFull:
           source.plannerSettings.allowUsedCupDropIfFull,
+        allowDiscardRetainedJuice:
+          source.plannerSettings.allowDiscardRetainedJuice,
       },
     },
     after: {
@@ -100,6 +103,8 @@ function draftFromBasis(
           source.plannerSettings.reservedJuiceJarSlots,
         allowUsedCupDropIfFull:
           source.plannerSettings.allowUsedCupDropIfFull,
+        allowDiscardRetainedJuice:
+          source.plannerSettings.allowDiscardRetainedJuice,
       },
     },
     changes: {
@@ -124,6 +129,7 @@ function draftFromBasis(
         droppedUsedCups: 0,
       },
       juiceJars: [],
+      discardedJuice: [],
       newlySuppliedCustomerIds: [],
     },
   }
